dbcc77855d21fcfd0c2d4d650c90b91229810ca1
Abishalini1602 <abiabishalini1002@gmail.com>
2025-06-08 14:50:08 +0530

    add the credit controller api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,84 +2,89 @@ import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+const registerUser = async (req, res) => {
+   try {
+      const { name, email, password } = req.body;
 
-const registerUser = async (req, res) =>{
-    try {
-        const { name,email,password} = req.body;
+      if (!name || !email || !password) {
+         return res.json({ success: false, message: "Missing Details" });
+      }
 
-        if(!name || !email || !password){
-            return res.json({success:false, message:"Missing Details"})
-        }
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
-        const salt = await bcrypt.genSalt(10)
-        const  hashedPassword = await bcrypt.hash(password, salt)
+      const userData = {
+         name,
+         email,
+         password: hashedPassword
+      };
 
+      const newUser = new userModel(userData);
+      const user = await newUser.save();
 
-        const  userData = {
-           name,email,password : hashedPassword
-        }
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-        const newUser = new userModel(userData)
-        const user = await newUser.save()
+      res.json({
+         success: true,
+         token,
+         user: { name: user.name }
+      });
 
+   } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+   }
+};
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+const loginUser = async (req, res) => {
+   try {
+      const { email, password } = req.body;
 
-        res.json({success: true, token, user:{name : user.name}})
+      const user = await userModel.findOne({ email });
 
+      if (!user) {
+         return res.json({ success: false, message: "User does not exist" });
+      }
 
-    } catch (error) {
-        console.log(error)
-        res.json({success: false, message: error.message})
-        
-    }
+      const isMatch = await bcrypt.compare(password, user.password);
 
-}
+      if (isMatch) {
+         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+         res.json({
+            success: true,
+            token,
+            user: { name: user.name }
+         });
+      } else {
+         return res.json({ success: false, message: "Invalid credentials" });
+      }
 
-const loginUser = async(req, res)=>{
-    try {
-        const { email, password} = req.body;
-        const user = await userModel.findOne({email})
+   } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+   }
+};
 
-        if(!user){
-            return res.json({success:false, message:"User does not exist"})
-        }
+const useCredits = async (req, res) => {
+   try {
+      const userId = req.user.id;
 
-     const  isMatch = await bcrypt.compare(password, user.password)
+      const user = await userModel.findById(userId);
 
-     if(isMatch){
-         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+      if (!user) {
+         return res.json({ success: false, message: "User not found" });
+      }
 
-        res.json({success: true, token, user:{name : user.name}})
+      res.json({
+         success: true,
+         credits: user.creditBalance,
+         user: { name: user.name }
+      });
 
+   } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+   }
+};
 
-
-     }else{
-        return res({success:false, message:"Invalid credebtials"})
-  
-     }
-
-    } catch (error) {
-
-        console.log(error)
-        res.json({success: false, message: error.message})
-        
-    }
-
-}
-
-const useCredits = async (req,res)=>{
-    try {
-        const {userId} = req.body
-
-        const user = await userModel.findById(userId)
-        res.json({success:true, credits : user.creditBalance, user : {name: user.name} })
-    } catch (error) {
-        console.log(error)
-        res.json({success: false, message: error.message})
-        
-    }
-
-}
-
-export {registerUser, loginUser}
+export { registerUser, loginUser, useCredits };
